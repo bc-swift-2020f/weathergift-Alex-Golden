33ee68d21be550f2dbd298f38853f19d177b58da
Alex Golden <goldenah@bc.edu>
2020-10-06 23:31:46 -0400

    6.2 commit

---
+++ WeatherGift Demo/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift Demo
+//
+//  Created by Alex Golden on 10/6/20.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    
+}
