f8a216112ccc5d89285761260793c03a9076fce5
Alex Golden <goldenah@bc.edu>
2020-10-08 16:01:37 -0400

    with .gitignore

---
+++ WeatherGift Demo/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  WeatherGift Demo
+//
+//  Created by Alex Golden on 10/8/20.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyC-GFq2U2d2sE5DsqDJAKyCNeq-JxEwDQU"
+}
